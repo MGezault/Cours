f8def18aef7f66b0c7cd276083c3fef29544039a
MGezault <mateo.gezault@etu.univ-orleans.fr>
2024-09-06 17:42:26 +0200

    Add files via upload

---
+++ Cours/Base de données/tp1.sql
@@ -0,0 +1,14 @@
+create table BdHabitantVoiture(
+Prenom Varchar2(20),
+Nom Varchar2(20),
+Sexe Varchar2(1),
+Rue Varchar2(20),
+Num Number(2),
+Ville Varchar2(10),
+MarqueV Varchar2(10),
+NomV Varchar2(10),
+AnneeV Number(4),
+CouleurV Varchar2(10),
+TypeMoteurV Varchar2(10),
+ImmV Varchar2(10)
+);
